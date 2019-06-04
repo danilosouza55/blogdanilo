744fe835f390821f21ab30505b381f7f5e565c08
danilo <danilo.souza@edu.unipar.br>
2019-06-03 21:34:31 -0300

    inicial

---
+++ pages/home.php
@@ -0,0 +1,95 @@
+<div id="container">
+    <!-- carrousel -->
+    <div id="myCarousel" class="carousel slide wow fadeIn" data-ride="carousel">
+        <!-- Indicators -->
+        <ol class="carousel-indicators">
+            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+            <li data-target="#myCarousel" data-slide-to="1"></li>
+            <li data-target="#myCarousel" data-slide-to="2"></li>
+        </ol>
+
+        <!-- Wrapper for slides -->
+        <div class="carousel-inner">
+            <div class="item active">
+                <img src="images/1.jpg" alt="New York">
+            </div>
+
+            <div class="item">
+                <img src="images/2.jpg" alt="Michel">
+            </div>
+
+            <div class="item">
+                <img src="images/3.jpg" alt="Festa">
+            </div>
+        </div>
+
+        <!-- Left and right controls -->
+        <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+            <span class="glyphicon glyphicon-chevron-left"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="right carousel-control" href="#myCarousel" data-slide="next">
+            <span class="glyphicon glyphicon-chevron-right"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
+    <!-- final do carrousel -->
+</div>
+<div class="container">
+    <div class="row services-row hover08 column wow fadeIn">
+        <legend>
+        <h2 id="demo08" class="text-center"><i class="fa fa-camera" aria-hidden="true"></i> Life</h2>
+        </legend>
+        <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4 mb-30">
+            <figure>
+                <a class="example-image-link" href="images/home_1.jpg" data-lightbox="example-1">
+                    <img class="example-image" src="images/home_1.jpg" alt="banner" />
+                </a>
+            </figure>
+            <span>Banner</span>
+
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4 mb-30">
+            <figure>
+                <a class="example-image-link" href="images/home_2.jpg" data-lightbox="example-1">
+                    <img class="example-image" src="images/home_2.jpg" alt="banner 2" />
+                </a>
+            </figure>
+            <span>Banner 1</span>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4 mb-30">
+            <figure>
+                <a class="example-image-link" href="images/home3_3.jpg" data-lightbox="example-1">
+                    <img class="example-image" src="images/home_3.jpg" alt="viagem" />
+                </a>
+            </figure>
+            <span>Viagem</span>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4 mb-30">
+            <figure>
+                <a class="example-image-link" href="images/img_4.jpg" data-lightbox="example-1">
+                    <img class="example-image" src="images/img_4.jpg" alt="terra" />
+                </a>
+            </figure>
+            <span>Terra</span>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4 mb-30">
+            <figure>
+                <a class="example-image-link" href="images/img_5.jpg" data-lightbox="example-1">
+                    <img class="example-image" src="images/img_5.jpg" alt="moto" />
+                </a>
+            </figure>
+            <span>Moto</span>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4 mb-30">
+            <figure>
+                <a class="example-image-link" href="images/img_6.png" data-lightbox="example-1">
+                    <img class="example-image" src="images/img_6.png" alt="esperanca" />
+                </a>
+            </figure>
+            <span>Em busca da felicidade</span>
+        </div>
+
+    </div>
+</div>
+</div> 
